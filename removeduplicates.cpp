b5e98b5adbb0d82353c61d4768302fcf1eff30a8
ljw <liamv7@gmail.com>
2021-08-29 21:37:23 +0900

    start

---
+++ removeduplicates.cpp
@@ -0,0 +1,37 @@
+//
+// Created by ljw on 27/08/21.
+//
+
+#include <vector>
+#include <iterator>
+#include <iostream>
+
+namespace RemoveDuplicates {
+    using namespace std;
+
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        int previous = nums[0];
+        size_t k = nums.size();
+        vector<int>::iterator newEnd = nums.end();
+        for (auto it = next(nums.begin()); it < newEnd;) {
+            auto destIt = it;
+            while (it < newEnd && *it == previous) {
+                it++;
+                //= prev(newEnd);
+                k--;
+            }
+            // at this point it will be different from previous
+            if (destIt < it) {
+                copy(it, newEnd, destIt);
+                newEnd = next(nums.begin(), k);
+            }
+            if (it < newEnd) {
+                previous = *it;
+                it++;
+            }
+        }
+        return k;
+    }
+}
---
+++ removeduplicates.h
@@ -0,0 +1,12 @@
+//
+// Created by ljw on 27/08/21.
+//
+#ifndef MYPRACT_REMOVEDUPLICATES_H
+#define MYPRACT_REMOVEDUPLICATES_H
+
+#include <vector>
+
+namespace RemoveDuplicates {
+    int removeDuplicates(std::vector<int>& nums);
+}
+#endif //MYPRACT_REMOVEDUPLICATES_H
