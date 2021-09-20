a81d92503e6a707ba2c333e056e5df57b15e4c81
ljw <liamv7@gmail.com>
2021-09-20 11:21:32 +0900

    rev

--- FindIfPathExistsGraph.cpp
+++ FindIfPathExistsGraph.cpp
@@ -4,6 +4,7 @@
 
 #include <unordered_set>
 #include <queue>
+#include <iostream>
 #include "FindIfPathExistsGraph.h"
 
 //  https://leetcode.com/problems/find-if-path-exists-in-graph/
@@ -23,22 +24,27 @@ namespace  FindIfPathExistsGraph {
         std::vector<std::unordered_set<int>> adjacency;
     };
     bool validPath(int n, std::vector<std::vector<int>>& edges, int start, int end) {
+        std::cout << "validPath " << n << "start: " << start << "end: " << end << "\n";
         Graph graph(n, edges);
-        std::vector<bool> mark(n, false);
+        std::vector<bool> markVisited(n, false);
         std::queue<int> q;
+        markVisited[start] = true;
         q.push(start);
         while (!q.empty()) {
             auto node = q.front();
             q.pop();
-            if (mark[node])
-                continue;
-
+            std::cout << "node " << node << "\n";
             for (auto n1: graph.adjacency[node]) {
-                if (n1 == end)
+
+                if (n1 == end) {
+                    std::cout << "end found " << n1;
                     return true;
-                q.push(n1);
+                }
+                if (!markVisited[n1]) {
+                    markVisited[n1] = true;
+                    q.push(n1);
+                }
             }
-            mark[node] = true;
         }
         return false;
     }
--- tests/FindIfPathExistsGraphTest.cpp
+++ tests/FindIfPathExistsGraphTest.cpp
@@ -14,7 +14,14 @@ TEST(FindIfPathExistsGraph, TEST1 ) {
 
 TEST(FindIfPathExistsGraph, Test2 ) {
     std::vector<std::tuple<int, int>> adjacencyList
-            {{0, 1}, {0, 2}, {3, 5,}, {5, 4}, {4, 3}};
-    auto adjacency = FindIfPathExistsGraph::generate(6, adjacencyList);
-    EXPECT_EQ(FindIfPathExistsGraph::validPath(6, adjacency, 0, 5), false);
+            {{0, 1}, {1, 2}, {2, 3,}};
+    auto adjacency = FindIfPathExistsGraph::generate(4, adjacencyList);
+    EXPECT_TRUE(FindIfPathExistsGraph::validPath(6, adjacency, 0, 3));
+}
+
+TEST(FindIfPathExistsGraph, Test3 ) {
+    std::vector<std::tuple<int, int>> adjacencyList
+            {{0, 1}, {1, 2}, {2, 0}, {2, 3}};
+    auto adjacency = FindIfPathExistsGraph::generate(4, adjacencyList);
+    EXPECT_TRUE(FindIfPathExistsGraph::validPath(6, adjacency, 0, 3));
 }
